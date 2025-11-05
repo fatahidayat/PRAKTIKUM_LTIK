c830c2a908e6f66e194b5a3ed979f905870a1929
fatahidayat <fatahnurhidayat61@gmail.com>
2025-11-05 14:13:41 +0700

    upload code daspro

---
+++ kondisional.py
@@ -0,0 +1,21 @@
+x = 1
+if ( x > 0 ):
+    print ( 1 > 0 )
+    y = x + 5
+    print(y)
+
+x = 0
+if ( x > 0 ):
+    print ( 2 > 0 )
+elif ( x < 0 ):
+    print(5 < 0)
+else :
+    print(x)
+
+a = 10 
+if ( a > 0 ):
+    print('nilai a lebih besat dari 0')
+    if (a > 5):
+        print('nilai a juga lebih besar dari 5')
+    else:
+        print('nilai a lebih kecil dari 5')
